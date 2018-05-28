dc200a3c5507dcbd111fc4f48f866546b6160bc3
Hellen Lee <hleexx@gmail.com>
2018-05-28 17:14:59 -0400

    docs: Add explanation comments for each step

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,8 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+
+         // Loops through each of the feed's (array) URLs to check that they are defined and are not empty
          it('has a URL defined and the URL is not empty', function() {
             for (let i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].url).toBeDefined();
@@ -43,6 +45,8 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         // Loops through each of the feed's (array) names to check that they are defined and are not empty
          it('has a name defined and the name is not empty', function() {
             for (let i = 0; i < allFeeds.length; i++) {
                 expect(allFeeds[i].name).toBeDefined();
@@ -60,6 +64,8 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+
+         // Checks that the body element has the 'menu-hidden' class
          it('is hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
          });
@@ -69,11 +75,14 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+          // Checks that when the menu-icon-link class is clicked, the body element does not have the 'menu-hidden' class AKA is visible
           it('changes visibility when the menu icon is clicked', function() {
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(false);
           });
 
+          // Checks that when the menu-icon-link class is clicked again, the body element DOES have the 'menu-hidden' class AKA is not visible
           it('hides when clicked again', function() {
             $('.menu-icon-link').trigger('click');
             expect($('body').hasClass('menu-hidden')).toBe(true);
@@ -90,10 +99,12 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+         // Loads the feed asynchronously 
          beforeEach(function(done) {
             loadFeed(0, done);
          });
 
+         // Checks that at least one entry inside the feed container has content (greater than 0)
          it('at least one entry element within feed container', function() {
             expect($('.feed .entry').length).toBeGreaterThan(0);
          });
@@ -108,10 +119,12 @@ $(function() {
          * Remember, loadFeed() is asynchronous.
          */
 
+         // Created a variable for the oldFeed
          let oldFeed;
+
+         // Everything in the function will load AFTER (similar to a setTimeOut) so that 'done' is called only once and finishes after everything loads
          beforeEach(function(done) {
-            // loads a web request and it puts in all the stuff into the feed class div
-            // everything in the function will load after similar to a setTimeOut
+            // loadFeed - Loads a web request and it puts in all the stuff into the feed class div
             loadFeed(0, function() {
                 oldFeed = $('.feed').html();
                 loadFeed(1, done);
@@ -119,6 +132,7 @@ $(function() {
 
          });
 
+         // Checks that the oldFeed has changed to show the new feed content
          it('loads new content when a new feed is loaded', function() {
             expect(oldFeed).not.toEqual($('.feed').html())
          });
